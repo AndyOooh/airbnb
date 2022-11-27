561bb38a7cbda7049df1628af96628f1d2e17e21
AndyOoh <andy.oeee@gmail.com>
2022-11-28 04:40:50 +0700

    sort mongo query not working properly

--- server/src/controlles/airbnb.controller.ts
+++ server/src/controlles/airbnb.controller.ts
@@ -3,21 +3,23 @@ import { collections } from '../config/mongoose';
 
 const getListings = async (req: Request, res: Response) => {
   console.log('hi from getListings');
-  const { page, perPage } = req.query;
-  if (!page || !perPage) {
-    return res.status(400).send('Missing page or perPage query params');
-  }
-  const skip: number = (+page - 1) * +perPage;
-
-  const query = {};
-  const totalMatches = await collections.abbListings?.countDocuments(query);
+  const page = Number(req.query.page) || 1;
+  const perPage = Number(req.query.perPage) || 6;
+  const sort = (req.query.sort as string) || 'price';
   console.log(
-    '🚀 ~ file: airbnb.controller.ts ~ line 13 ~ totalMatches',
-    totalMatches,
+    '🚀 ~ file: airbnb.controller.ts ~ line 9 ~ sort',
+    sort,
+    typeof sort,
   );
 
+  const skip: number = (page - 1) * perPage;
+  const query = {};
+  const totalMatches = await collections.abbListings?.countDocuments(query);
+
   const listings = await collections.abbListings
-    ?.find()
+    ?.find({}, { allowDiskUse: true })
+    .sort(sort, -1)
+    .allowDiskUse()
     .skip(skip)
     .limit(+perPage)
     .toArray();
